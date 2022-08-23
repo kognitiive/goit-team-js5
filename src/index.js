6cb4ad9e8f287a80b30ca6568162dbe99877e711
Evhenii Danchenko <danchenkoevhenii@gmail.com>
2022-08-24 01:43:04 +0300

    add test responce

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import templateFunction from './templates/card.hbs';
+const axios = require('axios').default;
 console.log(templateFunction({
     people: [
         "Yehuda Katz",
@@ -6,3 +7,12 @@ console.log(templateFunction({
         "Charles Jolley",
     ],
 }));
+async function getUser() {
+  try {
+    const response = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=28f59146d010acf01a886226973a360d');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+getUser();
