05dd272c2540d0d66c9f6996033623017aee8b19
ukrbride4u <ukrbride4u@gmail.com>
2022-08-25 15:13:30 +0300

    movies render

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import templateFunction from './templates/card.hbs';
 const axios = require('axios').default;
-const wraper = document.querySelector('.div')
+const wraper = document.querySelector('.div');
+import Notiflix from 'notiflix';
+import fetchFilms from './js/fetchFilms';
 // console.log(templateFunction({
 //   people: [
 //     "Yehuda Katz",
@@ -8,35 +10,50 @@ const wraper = document.querySelector('.div')
 //     "Charles Jolley",
 //   ],
 // }));
-async function getUser() {
-  try {
+
+const films = fetchFilms()
+  .then(result => {
+    console.log(result);
+    const render = result.map(item => templateFunction(item)).join('');
+    wraper.insertAdjacentHTML('beforeend', render);
+  });
+
+  async function getUser() {
+    try {
     const response = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=28f59146d010acf01a886226973a360d');
-    console.log(response)
-    const card = response.data.results[1]
-    console.log(card)
+    // console.log(response)
+     const card = response.data.results[1]
+     // console.log(card)
     const { poster_path, title, release_date, vote_average, genre_ids } = card;
-    let genre = []
-    if (genre_ids.length <= 2) { genre = [...genre_ids] }
-    else {
-      genre = [...genre_ids].slice(0, 2);
-      genre.push('Other');
-      genre.join(", ");
+     let genre = []
+     if (genre_ids.length <= 2) { genre = [...genre_ids] }
+      else {
+        genre = [...genre_ids].slice(0, 2);
+        genre.push('Other');
+       genre.join(", ");
+     }
+      return { poster_path, title, release_date, vote_average, genre }
+   } catch (error) {
+     console.error(error);
     }
-    return { poster_path, title, release_date, vote_average, genre }
-  } catch (error) {
-    console.error(error);
   }
-}
 
-async function makeMarkup() { 
-  try {
-    const data = await getUser();
-    wraper.innerHTML = templateFunction(data);
+  async function makeMarkup() {
+   try {
+       const data = await getUser();
+      wraper.innerHTML = templateFunction(data);
 
-  }
-  catch { 
-    console.error(error);
-  }
-}
+     }
+     catch {
+       console.error(error);
+     }
+   }
 
-makeMarkup();
+//  makeMarkup();
+
+
+
+
+
+
+
