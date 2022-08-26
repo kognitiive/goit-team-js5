51a0d76a3b1a144fea38806164e06a21910ebc2b
Evhenii Danchenko <danchenkoevhenii@gmail.com>
2022-08-26 10:13:45 +0300

    Update fetchGenres.js

--- src/js/fetchGenres.js
+++ src/js/fetchGenres.js
@@ -5,7 +5,7 @@ export function fetchGenres(array) {
     else {
         genre = [...array].slice(0, 2);
     }
-    newGenre = []
+    const newGenre = []
     for (item of arrayGanre) {
         genre.map((genr) => {
             if (genr === item.id) { newGenre.push(item.name) }
