0919c37a6f0a44809d177b4b256655991e5604ca
ukrbride4u <ukrbride4u@gmail.com>
2022-08-29 00:29:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -28,16 +28,45 @@ async function makeMarkup(currentPage) {
   paginat.pagMake();
 }
 
-makeMarkup(currentPage).then(r => {
-  modal()
-});
+makeMarkup(currentPage);
+//   .then(r => {
+//   modal()
+// });
+const backdrop = document.querySelector('.window-backdrop');
+const modalBtnClose = document.querySelector('.js-modal-close');
   const galleryRef = document.querySelector('.film_list')
   galleryRef.addEventListener('click', openModal);
-  const instance = basicLightbox.create(document.querySelector('#modal-window'))
-  instance.show();
-}
+const instance = basicLightbox.create(document.querySelector('#modal-window'),
 
-);
+  {
+    onShow: () => {
+      backdrop.classList.add('.active');
+      window.addEventListener('keydown', onEscapeButtonClick);
+      backdrop.addEventListener('click', onBackDropClick);
+      modalBtnClose.addEventListener('click', onModalBtnClose)
+    },
+    onClose: () => {
+      window.removeEventListener('keydown', onEscapeButtonClick);
+      backdrop.removeEventListener('click', onBackDropClick);
+      modalBtnClose.removeEventListener('click', onModalBtnClose);
+    }
+});
+instance.show();
+
+function onEscapeButtonClick(e) {
+  if (e.code === 'Escape') {
+    backdrop.classList.remove('.active');
+          instance.close();
+        }
+}
+function onBackDropClick(e) {
+  backdrop.classList.remove('.active');
+  instance.close();
+}
+function onModalBtnClose(e) {
+ backdrop.classList.remove('.active');
+  instance.close();
+}
 
 
 //Рендер при пошуку
