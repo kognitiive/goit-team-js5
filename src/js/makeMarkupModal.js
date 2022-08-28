dc75bddcd561e7f5c43084b432abac289b5f51cf
ukrbride4u <ukrbride4u@gmail.com>
2022-08-28 14:58:32 +0300

    fighting lightbox

--- src/js/makeMarkupModal.js
+++ src/js/makeMarkupModal.js
@@ -3,3 +3,5 @@ export default async  function makeMarkupModal(id) {
    const data = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=39e7639bbdd6f3ff4985c89cb032db66`)
   return {} = data.data;
 }
+
+
