3397b75d6074a6ab5cd8289f112f5debc3d18b54
Evhenii Danchenko <danchenkoevhenii@gmail.com>
2022-08-28 16:00:35 +0300

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -27,7 +27,7 @@ function openModal(e) {
 
 const modalWindow = document.querySelector('.modal__content');
 
-export default function modal () {
+function modal () {
 
   !function (e) { "function" != typeof e.matches && (e.matches = e.msMatchesSelector || e.mozMatchesSelector || e.webkitMatchesSelector || function (e) { for (var t = this, o = (t.document || t.ownerDocument).querySelectorAll(e), n = 0; o[n] && o[n] !== t;)++n; return Boolean(o[n]) }), "function" != typeof e.closest && (e.closest = function (e) { for (var t = this; t && 1 === t.nodeType;) { if (t.matches(e)) return t; t = t.parentNode } return null }) }(window.Element.prototype);
   // import modal from './templates/modal.hbs';
@@ -142,4 +142,4 @@ export default function modal () {
 // function onBtnCloseModalClick() {
 //   backdropEl.classList.add('is-hidden');
 //   window.removeEventListener('keydown', onKeyPress);
-// }
+}
