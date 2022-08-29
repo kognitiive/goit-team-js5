e2a0a5e3145d96830a8b5c72c1debf47bd5b5b1a
Evhenii Danchenko <danchenkoevhenii@gmail.com>
2022-08-30 00:27:55 +0300

    Update formRegistration.js

--- src/js/formRegistration.js
+++ src/js/formRegistration.js
@@ -5,14 +5,36 @@ const formLink = document.querySelector('.header-regist');
 formLink.addEventListener('click', openModal)
 
 const instance = basicLightbox.create(
-    `<div class='modal'><form class='regist-form' autocomplete="off"><h1 class='regist-title'>Please, fill in all fields</h1><label><span class="regist-label">Email</span><input class='regist-input' type="email" name="email" ></input></label><label><span class="regist-label">Password</span><input class='regist-input' type="text" name="password"></input></label><button type='submit' class='regist-btn'>Registration</button>
-    </form></div>`,
+    `<div class='registration-backdrop'>
+    <div class='modal'>
+    <form class='regist-form' autocomplete="off">
+        <h1 class='regist-title'>Please, fill in all fields</h1>
+            <label>
+                <span class="regist-label">Email</span>
+                <input class='regist-input' type="email" name="email" ></input>
+            </label>
+            <label>
+                <span class="regist-label">Password</span>
+                <input class='regist-input' type="text" name="password"></input>
+            </label>
+            <button type='submit' class='regist-btn'>Registration</button>
+    </form>
+    </div>`,
     {
         onShow: () => {
+            refs = {
+                submitBtn: document.querySelector('.regist-btn'),
+                backdrop: document.querySelector('.registration-backdrop'),
+                modal: document.querySelector('.modal')
+             }
             window.addEventListener('keydown', onEscCode)
+            refs.modal.classList.add('active')
+            refs.backdrop.classList.add('active')
         },
 
         onClose: () => {
+            refs.modal.classList.remove('active')
+            refs.backdrop.classList.remove('active')
             window.removeEventListener('keydown', onEscCode)
         }
     }
